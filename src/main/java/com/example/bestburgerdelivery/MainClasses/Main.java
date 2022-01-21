d11e73b6a01a2ce7afb41c4e6dac574d23a87615
George Baltzakis <george-baltzakis@hotmail.gr>
2022-01-21 05:56:09 +0200

    Changes

--- src/main/java/com/example/bestburgerdelivery/MainClasses/Main.java
+++ src/main/java/com/example/bestburgerdelivery/MainClasses/Main.java
@@ -2,25 +2,14 @@ package com.example.bestburgerdelivery.MainClasses;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.Properties;
+
 
 public class Main {
     public static void main(String[] args) {
         try {
             // Make the connection
-
-            /*String url = "jdbc:postgresql://localhost:5432/Best Burger Delivery";
-            Properties props = new Properties();
-            props.setProperty("user","Giorgos");
-            props.setProperty("password","1234");
-            props.setProperty("ssl","false");
-            Connection conn = DriverManager.getConnection(url, props);*/
             Class.forName("org.postgresql.Driver");
             Connection c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Best Burger Delivery", "Giorgos", "1234");
-
-
         }catch(Exception e){
             System.out.println("AdminD Error");
             e.printStackTrace();
