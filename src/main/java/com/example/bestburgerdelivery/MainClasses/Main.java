43493f374feed69c824daab4a85e912cccad650e
George Baltzakis <george-baltzakis@hotmail.gr>
2022-02-13 19:05:31 +0200

    Changes

--- src/main/java/com/example/bestburgerdelivery/MainClasses/Main.java
+++ src/main/java/com/example/bestburgerdelivery/MainClasses/Main.java
@@ -1,9 +1,6 @@
 package com.example.bestburgerdelivery.MainClasses;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 
 public class Main {
@@ -14,21 +11,19 @@ public class Main {
             Connection c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Best Burger Delivery", "Giorgos", "1234");
 
             // Make & Execute the Query
-            Statement query = c.createStatement();
-            ResultSet rs = query.executeQuery("select * from admin");
+            String sql = "select * from admin where name=?";
+            PreparedStatement p = c.prepareStatement(sql);
+            p.setString(1, "'Lexie");
+            ResultSet rs = p.executeQuery();
 
             // Take the Result of the Query and make a Admin
-            if(rs.next()){
+            while(rs.next()){
                 System.out.println(rs.getString("email") + " " +rs.getString("name"));
             }
 
             // Close Connection
             c.close();
 
-            // Print to console the created Admin
-            /*if(admin != null) {
-                System.out.println("Admin Exists");
-            }*/
         }catch(Exception e){
             System.out.println("AdminD Error");
             e.printStackTrace();
